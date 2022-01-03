4824958733146a242395711802d285728142ff36
pdziok <pdziok@users.noreply.github.com>
2022-01-03 18:15:20 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.9519135bb10966ee498bd5ac348dae4c.js
@@ -0,0 +1,23 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90c3bac413755cdc3cb89b3af26afaaf",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.4e275ca7db7f235c0409.bundle.js"
+  },
+  {
+    "revision": "2faa2fd236c45bd6d4c0",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.4e275ca7db7f235c0409.bundle.js"
+  },
+  {
+    "url": "vendors~main.4e275ca7db7f235c0409.bundle.js"
+  },
+  {
+    "revision": "4ec0d0272de672ae9269",
+    "url": "vendors~main.css"
+  }
+]);
